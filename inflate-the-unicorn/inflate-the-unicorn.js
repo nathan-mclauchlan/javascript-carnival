1ceb3ede435fe8c5556bfbcb660067b486cda35b
nathan-mclauchlan <nathanmclauchlan21@gmail.com>
2021-03-25 00:14:18 +1300

    Done steps 1-5

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -2,4 +2,20 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+document.getElementById("uni0").onclick = clicked
+document.getElementById("uni1").onclick = clicked
+document.getElementById("uni2").onclick = clicked
+
+var inflation = [0, 0, 0]
+
+function clicked(event) {
+  var unicorn = event.target
+  var id = unicorn.id[3]
+
+  inflation[id]++
+
+  if (inflation[id] == 4)
+      inflation[id] = 0
+
+    unicorn.src = "./images/unicorn-" + inflation[id] + ".png"
+}
