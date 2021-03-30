16bf89841615d5d28e902d072da51413b5932d2d
nathan-mclauchlan <nathanmclauchlan21@gmail.com>
2021-03-31 07:27:47 +1300

    added win pop up

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -7,15 +7,15 @@ document.getElementById("uni1").onclick = clicked
 document.getElementById("uni2").onclick = clicked
 
 var inflation = [0, 0, 0]
-
 function clicked(event) {
   var unicorn = event.target
   var id = unicorn.id[3]
 
+  
   inflation[id]++
-
-  if (inflation[id] == 4)
+  unicorn.src = "./images/unicorn-" + inflation[id] + ".png"
+  if (inflation[id] == 3) {
       inflation[id] = 0
-
-    unicorn.src = "./images/unicorn-" + inflation[id] + ".png"
+      alert("You Win")
+  }     
 }
